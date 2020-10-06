b7733c650c4ee5af27450c586c28ee3ca660bef1
Thiago Figueiredo <thi4go@Thiagos-MacBook-Air.local>
2020-10-06 16:34:18 -0300

    piwww: Add user proposal credits cmd and a few naming refactor

--- politeiawww/cmd/piwww/piwww.go
+++ politeiawww/cmd/piwww/piwww.go
@@ -44,23 +44,24 @@ type piwww struct {
 	Me      shared.MeCmd      `command:"me"`
 
 	// User commands
-	UserNew                userNewCmd                   `command:"usernew"`
-	UserEdit               userEditCmd                  `command:"useredit"`
-	UserManage             shared.UserManageCmd         `command:"usermanage"`
-	UserEmailVerify        userEmailVerifyCmd           `command:"useremailverify"`
-	UserVerificationResend userVerificationResendCmd    `command:"userverificationresend"`
-	UserPasswordReset      shared.UserPasswordResetCmd  `command:"userpasswordreset"`
-	UserPasswordChange     shared.UserPasswordChangeCmd `command:"userpasswordchange"`
-	UserUsernameChange     shared.UserUsernameChangeCmd `command:"userusernamechange"`
-	UserKeyUpdate          shared.UserKeyUpdateCmd      `command:"userkeyupdate"`
-	UserTOTPSet            shared.UserTOTPSetCmd        `command:"usertotpset"`
-	UserTOTPVerify         shared.UserTOTPVerifyCmd     `command:"usertotpverify"`
-	UserProposalPaywall    userProposalPaywallCmd       `command:"userproposalpaywall"`
-	UserPaymentVerify      userPaymentVerifyCmd         `command:"userpaymentverify"`
-	UserPaymentsRescan     userPaymentsRescanCmd        `command:"userpaymentsrescan"`
-	UserPendingPayment     userPendingPaymentCmd        `command:"userpendingpayment"`
-	UserDetails            userDetailsCmd               `command:"userdetails"`
-	Users                  shared.UsersCmd              `command:"users"`
+	UserNew                 userNewCmd                   `command:"usernew"`
+	UserEdit                userEditCmd                  `command:"useredit"`
+	UserManage              shared.UserManageCmd         `command:"usermanage"`
+	UserEmailVerify         userEmailVerifyCmd           `command:"useremailverify"`
+	UserVerificationResend  userVerificationResendCmd    `command:"userverificationresend"`
+	UserPasswordReset       shared.UserPasswordResetCmd  `command:"userpasswordreset"`
+	UserPasswordChange      shared.UserPasswordChangeCmd `command:"userpasswordchange"`
+	UserUsernameChange      shared.UserUsernameChangeCmd `command:"userusernamechange"`
+	UserKeyUpdate           shared.UserKeyUpdateCmd      `command:"userkeyupdate"`
+	UserTOTPSet             shared.UserTOTPSetCmd        `command:"usertotpset"`
+	UserTOTPVerify          shared.UserTOTPVerifyCmd     `command:"usertotpverify"`
+	UserRegistrationPayment userRegistrationPaymentCmd   `command:"userregistrationpayment"`
+	UserProposalPaywall     userProposalPaywallCmd       `command:"userproposalpaywall"`
+	UserProposalPaywallTx   userProposalPaywallTxCmd     `command:"userproposalpaywalltx"`
+	UserProposalCredits     userProposalCreditsCmd       `command:"userproposalcredits"`
+	UserPaymentsRescan      userPaymentsRescanCmd        `command:"userpaymentsrescan"`
+	UserDetails             userDetailsCmd               `command:"userdetails"`
+	Users                   shared.UsersCmd              `command:"users"`
 
 	// TODO some of the proposal commands use both the --unvetted and
 	// --vetted flags. Let make them all use only the --unvetted flag.
