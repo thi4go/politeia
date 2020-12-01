ea6c4f0a0c464589ebc820a75c6199b55fe9c802
thi4go <thiagofigueiredo777@gmail.com>
2020-12-01 18:07:42 -0300

    add all content verification tests and run them through New endpoint

--- politeiad/backend/tlogbe/tlogbe_test.go
+++ politeiad/backend/tlogbe/tlogbe_test.go
@@ -5,143 +5,43 @@
 package tlogbe
 
 import (
-	"fmt"
+	"errors"
 	"testing"
 
-	v1 "github.com/decred/politeia/politeiad/api/v1"
 	"github.com/decred/politeia/politeiad/backend"
 )
-
-type recordContentTest struct {
-	description string
-	metadata    []backend.MetadataStream
-	files       []backend.File
-	filesDel    []string
-	err         backend.ContentVerificationError
-}
-
-func setupRecordContentTests(t *testing.T) []recordContentTest {
-	t.Helper()
-
-	var rct []recordContentTest
-
-	// Invalid metadata ID error
-	md := []backend.MetadataStream{
-		newBackendMetadataStream(t, v1.MetadataStreamsMax+1, ""),
-	}
-	fs := []backend.File{
-		newBackendFile(t, "index.md"),
-	}
-	fsDel := []string{}
-	err := backend.ContentVerificationError{
-		ErrorCode: v1.ErrorStatusInvalidMDID,
-	}
-	rct = append(rct, recordContentTest{
-		description: "Invalid metadata ID error",
-		metadata:    md,
-		files:       fs,
-		filesDel:    fsDel,
-		err:         err,
-	})
-
-	// Duplicate metadata ID error
-	md = []backend.MetadataStream{
-		newBackendMetadataStream(t, 1, ""),
-		newBackendMetadataStream(t, 1, ""),
-	}
-	fs = []backend.File{
-		newBackendFile(t, "index.md"),
-	}
-	err = backend.ContentVerificationError{
-		ErrorCode: v1.ErrorStatusDuplicateMDID,
-	}
-	rct = append(rct, recordContentTest{
-		description: "Duplicate metadata ID error",
-		metadata:    md,
-		files:       fs,
-		filesDel:    fsDel,
-		err:         err,
-	})
-
-	// Invalid filename error
-	fs = []backend.File{
-		newBackendFile(t, "invalid/filename.md"),
-	}
-	md = []backend.MetadataStream{
-		newBackendMetadataStream(t, 1, ""),
-	}
-	err = backend.ContentVerificationError{
-		ErrorCode: v1.ErrorStatusInvalidFilename,
-	}
-	rct = append(rct, recordContentTest{
-		description: "Invalid filename error",
-		metadata:    md,
-		files:       fs,
-		err:         err,
-	})
-
-	// Empty files error
-	fs = []backend.File{}
-	md = []backend.MetadataStream{
-		newBackendMetadataStream(t, 1, ""),
-	}
-	err = backend.ContentVerificationError{
-		ErrorCode: v1.ErrorStatusEmpty,
-	}
-	rct = append(rct, recordContentTest{
-		description: "Empty files error",
-		metadata:    md,
-		files:       fs,
-		err:         err,
-	})
-
-	// Duplicate filename error
-	fs = []backend.File{
-		newBackendFile(t, "index.md"),
-	}
-	fsDel = []string{}
-	md = []backend.MetadataStream{
-		newBackendMetadataStream(t, 1, ""),
-	}
-	err = backend.ContentVerificationError{
-		ErrorCode: v1.ErrorStatusDuplicateFilename,
-	}
-	rct = append(rct, recordContentTest{
-		description: "Duplicate filename error",
-		metadata:    md,
-		files:       fs,
-		filesDel:    fsDel,
-		err:         err,
-	})
-
-	return rct
-}
 
 func TestNewRecord(t *testing.T) {
 	tlogBackend, err := newTestTlogBackend(t)
 	if err != nil {
-		fmt.Printf("Error in newTestTlogBackend %v", err)
-		return
+		t.Errorf("error in newTestTlogBackend %v", err)
 	}
 
-	metadata := backend.MetadataStream{
-		ID:      1,
-		Payload: "",
+	// Test all record content verification error through the New endpoint
+	recordContentTests := setupRecordContentTests(t)
+	for _, test := range recordContentTests {
+		t.Run(test.description, func(t *testing.T) {
+			_, err := tlogBackend.New(test.metadata, test.files)
+
+			var contentError backend.ContentVerificationError
+			if errors.As(err, &contentError) {
+				if contentError.ErrorCode != test.err.ErrorCode {
+					t.Errorf("got error %v, want %v", contentError.ErrorCode,
+						test.err.ErrorCode)
+				}
+			}
+		})
 	}
 
-	file := backend.File{
-		Name:    "index.md",
-		MIME:    "text/plain; charset=utf-8",
-		Digest:  "22e88c7d6da9b73fbb515ed6a8f6d133c680527a799e3069ca7ce346d90649b2",
-		Payload: "bW9vCg==",
+	// Test success case
+	md := []backend.MetadataStream{
+		newBackendMetadataStream(t, 1, ""),
 	}
-
-	rmd, err := tlogBackend.New([]backend.MetadataStream{metadata},
-		[]backend.File{file})
+	fs := []backend.File{
+		newBackendFile(t, "index.md"),
+	}
+	_, err = tlogBackend.New(md, fs)
 	if err != nil {
-		fmt.Printf("Error in New %v", err)
-		return
+		t.Errorf("success case failed with %v", err)
 	}
-
-	fmt.Println(rmd)
 }
